a7c618135b52ab18e24ee43b414758f6704f34dc
Kimberley Salter <ksalter@Kimberleys-MacBook-Pro.local>
2021-10-23 12:55:37 +0100

    REST first attempt

--- albums/views.py
+++ albums/views.py
@@ -14,3 +14,34 @@ def index(request):
     return render(request, 'albums/index.html', context)
 
     # add this first - return HttpResponse("HELLO WORLD")
+
+# API handlers
+def albums(request):
+    if request.method == 'GET':
+        return read(request)
+    if request.method == 'POST':
+        return create(request)
+
+def album(request):
+    if request.method == 'GET':
+        return read_one(request)
+    if request.method == 'PATCH':
+        return update(request)
+    if request.method == 'DELETE':
+        return delete(request)
+
+# CRUD
+def create(request):
+    return HttpResponse('CREATE')
+
+def read(request):
+    return HttpResponse('READ')
+
+def read_one(request):
+    return HttpResponse('READ ONE')
+
+def update(request):
+    return HttpResponse('UPDATE')
+
+def delete(request):
+    return HttpResponse('DELETE')
