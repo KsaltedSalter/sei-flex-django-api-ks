8cfe1c149c96f90a43741e7bfb2b8b2f7333ee0e
Kimberley Salter <ksalter@Kimberleys-MacBook-Pro.local>
2021-10-23 15:34:05 +0100

    added CRUD test with postman

--- albums/urls.py
+++ albums/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index, AlbumListView
+from .views import AlbumDetailView, index, AlbumListView
 
 urlpatterns = [
     path("", index),
     path("api/", AlbumListView.as_view()),
+    path("api/<int:id>/", AlbumDetailView.as_view()),
 ] 
--- albums/views.py
+++ albums/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from rest_framework import views, response, status
+from rest_framework import views, response, status, exceptions
 
 from albums.serializers import AlbumSerializer
-
 from .models import Album
 
 # Create your views here.
@@ -33,6 +32,38 @@ class AlbumListView(views.APIView):
             return response.Response(album_to_add.data, status=status.HTTP_201_CREATED)
 
         return response.Response(album_to_add.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class AlbumDetailView(views.APIView):
+    def get_album_by_id(self, id):
+        try:
+            return Album.objects.get(id=id)
+        except Album.DoesNotExist:
+            raise exceptions.NotFound(detail="Album does not exist")
+
+    def get(self, request, id):
+        album = self.get_album_by_id(id)
+        serialized_album = AlbumSerializer(album)
+        return response.Response(serialized_album.data, status=status.HTTP_200_OK)
+
+    def delete(self, request, id):
+        album = self.get_album_by_id(id)
+        album.delete()
+        return response.Response(status=status.HTTP_204_NO_CONTENT)
+
+    def put(self, request, id):
+        album = self.get_album_by_id(id)
+        updated_album = AlbumSerializer(album, data=request.data)
+        if updated_album.is_valid():
+            updated_album.save()
+            return response.Response(updated_album.data, status=status.HTTP_201_CREATED)
+        return response.Response(updated_album.errors, status=status.HTTP_400_BAD_REQUEST) 
+
+
+
+
+
+
+
 
 
 # WORKINGS OF WHAT NEEDS TO HAPPEN SUDO-CODE
