c63d003d2563c4f8405924f7d07a6bc5bae46fef
Kimberley Salter <ksalter@Kimberleys-MacBook-Pro.local>
2021-10-23 12:19:41 +0100

    added and migrated model

--- albums/views.py
+++ albums/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import Album
 
 # Create your views here.
+def index(request):
+    # grabbing what you need from the database
+    list = Album.objects.all
+    # creating a context object
+    context = {'albums': list}
+
+    # rendering based on a template
+    return render(request, 'albums/index.html', context)
+
+    # add this first - return HttpResponse("HELLO WORLD")
