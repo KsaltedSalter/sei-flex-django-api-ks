29b26f39e44898be06481a9b0062134bed4bd4df
Kimberley Salter <ksalter@Kimberleys-MacBook-Pro.local>
2021-10-26 19:04:37 +0100

    removed artist field string from albums model

--- albums/models.py
+++ albums/models.py
@@ -5,7 +5,6 @@ from artists.models import Artist
 # Create your models here.
 class Album(models.Model):
     title = models.CharField(max_length=50)
-    artist = models.CharField(max_length=50)
     cover_image = models.CharField(max_length=200)
     album_artist = models.ForeignKey(Artist, on_delete=models.CASCADE, null=True, default=None)
 
